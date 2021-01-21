69fe7ba16ea3f43648038272d6a20cdf7aec610d
Nouman Baloch <nomibaloch775@hotmail.com>
2021-01-21 22:05:31 +0500

    Initial Commit.

---
+++ eShop/eShop.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="eShop.WebUI.MvcApplication" Language="C#" %>
---
+++ eShop/eShop.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eShop.WebUI.Startup))]
+namespace eShop.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
