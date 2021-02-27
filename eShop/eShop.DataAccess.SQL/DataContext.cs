334dc0a6f83ba3823118f11fbabd405bdcad0499
noumanbaloch <noumanbaloch@NOUMANBALOC3171>
2021-02-27 20:52:43 +0500

    Added Entity Framework

---
+++ eShop/eShop.DataAccess.SQL/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using eShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop.DataAccess.SQL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("EcomDBConnectionString")
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+
+    }
+}
---
+++ eShop/eShop.DataAccess.SQL/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace eShop.DataAccess.SQL.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<eShop.DataAccess.SQL.DataContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(eShop.DataAccess.SQL.DataContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
