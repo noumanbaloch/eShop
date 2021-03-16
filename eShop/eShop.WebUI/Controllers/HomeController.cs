e5074a00eb1599fbd9110e376efaff74d17d408f
noumanbaloch <noumanbaloch@NOUMANBALOC3171>
2021-03-16 23:46:00 +0500

    Product Listing Added

--- eShop/eShop.WebUI/Controllers/HomeController.cs
+++ eShop/eShop.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using eShop.Core.Contracts;
+using eShop.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +10,31 @@ namespace eShop.WebUI.Controllers
 {
     public class HomeController : Controller
     {
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> productCategoryContext)
+        {
+            context = productContext;
+            productCategories = productCategoryContext;
+        }
         public ActionResult Index()
         {
-            return View();
+            List<Product> products = context.Collection().ToList();
+            return View(products);
         }
 
+        public ActionResult Details(string Id)
+        {
+            Product product = context.Find(Id);
+            if(product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(product);
+            }
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
