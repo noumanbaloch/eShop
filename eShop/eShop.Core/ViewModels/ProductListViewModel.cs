b1628559539ad07622c126b49563bf987e1ec57e
noumanbaloch <noumanbaloch@NOUMANBALOC3171>
2021-03-17 22:27:39 +0500

    Product Filter Added

---
+++ eShop/eShop.Core/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using eShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop.Core.ViewModels
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public IEnumerable<ProductCategory> ProductCategories { get; set; }
+    }
+}
