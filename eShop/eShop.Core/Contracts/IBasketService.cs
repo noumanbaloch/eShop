0c400575e151f2d4d35e4d8d0b800ed0dc6449d7
noumanbaloch <noumanbaloch@NOUMANBALOC3171>
2021-04-02 22:20:03 +0500

    Basket Service Work is Completed!

---
+++ eShop/eShop.Core/Contracts/IBasketService.cs
@@ -0,0 +1,18 @@
+﻿using eShop.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace eShop.Core.Contracts
+{
+    public interface IBasketService
+    {
+        void AddToBasket(HttpContextBase httpContext, string productId);
+        List<BasketItemViewModel> GetBasketItems(HttpContextBase httpContext);
+        BasketSummaryViewModel GetBasketSummary(HttpContextBase httpContext);
+        void RemoveFromBasket(HttpContextBase httpContext, string itemId);
+    }
+}
